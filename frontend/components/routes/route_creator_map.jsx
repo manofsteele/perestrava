3470f7af41c2c579eb59604565dfad6e4b1970f8
Jeremiah Steele <jeremiahsteele@gmail.com>
2018-08-11 23:06:30 -0700

    Make letter show up on pointers on map

--- frontend/components/routes/route_creator_map.jsx
+++ frontend/components/routes/route_creator_map.jsx
@@ -12,11 +12,37 @@ const mapOptions = {
   zoom: 13
 };
 
+//Much of the below is from the Google Maps API, and slightly modified
+
 class RouteCreatorMap extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.placeMarkerAndPanTo = this.placeMarkerAndPanTo.bind(this);
+
+
+  }
+
 
   componentDidMount() {
     const map = this.refs.map;
     this.map = new google.maps.Map(map, mapOptions);
+
+    this.map.addListener('click', (e) => {
+      this.placeMarkerAndPanTo(e.latLng, map);
+    });
+    this.labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.labelIndex = 0;
+  }
+
+  placeMarkerAndPanTo(latLng, map) {
+    let marker = new google.maps.Marker({
+      position: latLng,
+      animation: google.maps.Animation.DROP,
+      label: this.labels[this.labelIndex++ % this.labels.length],
+    });
+    this.map.panTo(latLng);
+    marker.setMap(this.map);
   }
 
   render() {
