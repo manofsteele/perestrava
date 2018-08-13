d2c1da07affa1a07a9a401240cf38dcb35eec922
Jeremiah Steele <jeremiahsteele@gmail.com>
2018-08-12 20:08:51 -0700

    Add toolbars (non-functional for now)

--- frontend/components/routes/route_creator_map.jsx
+++ frontend/components/routes/route_creator_map.jsx
@@ -4,12 +4,26 @@ import { withRouter } from 'react-router-dom';
 
 // Thank you BenchBnB for the below
 
+const mapStyles = {
+  hide: [
+  {featureType: "poi",
+  elementType: "labels",
+  stylers: [{visibility: "off"}]
+}
+]
+};
+
 const mapOptions = {
   center: {
     lat: 37.773942,
     lng: -122.431297
   },
-  zoom: 13
+  zoom: 13,
+  clickableIcons: false,
+  styles: mapStyles['hide'],
+  zoomControlOptions: {
+             position: google.maps.ControlPosition.LEFT_TOP
+  }
 };
 
 //Much of the below is from the Google Maps API, and slightly modified
@@ -48,8 +62,45 @@ class RouteCreatorMap extends React.Component {
   render() {
 
     return (
-      <div className="map" ref="map">
-        Map
+      <div className="map-container">
+        <div className="map-tool-bar">
+          <input className="location-search"
+            placeholder="Enter a location"
+            type="text" ></input>
+          <div className= "search-button" title="Search">
+            <div className="search-icon"></div>
+          </div>
+          <div className="button" title="Undo last marker">
+            <div className="button-label">Undo</div>
+          </div>
+          <div className="button" title="Redo last marker">
+            <div className="button-label">Redo</div>
+          </div>
+          <div className="button" title="Clear all markers">
+            <div className="button-label">Clear</div>
+          </div>
+          <div className="button" title="Ride">
+            <div className="button-label">Ride</div>
+          </div>
+          <div className="button" title="Run">
+            <div className="button-label">Run</div>
+          </div>
+          <div className="float-right">
+            <div className="button-save" title="Save">
+              <div className="button-save-label">Save</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="map" ref="map">
+          Map
+
+        </div>
+        <div className="route-stats-bar">
+          <ul>
+
+          </ul>
+        </div>
       </div>
     );
   }
