ca340a575f2773f5296f882de41a306a4cbe6fb6
Jeremiah Steele <jeremiahsteele@gmail.com>
2018-09-18 12:00:05 -0700

    Add favicon and change layout of splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -5,7 +5,6 @@ const Splash = () => (
   <div className="splash">
 
     <h1>Welcome to Perestrava, the top site for cyclists and runners to track their routes and workouts.</h1>
-    <img className="splash_photo" src={window.bike_under_banyans}/>
   </div>
 );
 
