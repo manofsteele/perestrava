187a7abff013467905833ef0b88dfb05c243b7e7
Jeremiah Steele <jeremiahsteele@gmail.com>
2018-09-19 15:33:25 -0700

    Fix layout on not_found page

--- frontend/components/not_found.jsx
+++ frontend/components/not_found.jsx
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom';
 const NotFound = () => (
   <div className="not-found-container">
     <h1>Sorry, the page you were looking for was not found.</h1>
-    <center><Link className="not_found" to="/">Return to Home Page</Link></center>
-    <img className="not-found" src={window.not_found}/>
+    <center><Link className="not_found" to="/">Click here to return to Home Page</Link></center>
   </div>
 );
 
